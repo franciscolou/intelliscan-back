8b8956376bf8fca2d00769c28824875e6cedc3bf
Francisco Lou <franciscolou@tecgraf.puc-rio.br>
2025-03-17 03:26:04 -0300

    Return port back to 3000

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
