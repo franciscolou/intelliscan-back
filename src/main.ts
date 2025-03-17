ac6102db9491d836020d40cd639f9cb816e6424d
Francisco Lou <franciscolou@tecgraf.puc-rio.br>
2025-03-17 01:51:00 -0300

    Attempt to remove frontend URL from origin field in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'intelliscan-inky.vercel.app',
+    origin: '',
     methods: 'GET, POST, PATCH, DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
