54b955f0885909b887ec8b6ce396ff24fa1d32e8
Francisco Lou <franciscogardenberg@gmail.com>
2025-03-17 12:53:29 -0300

    Revert back to more generic CORS restriction

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'https://intelliscan-inky.vercel.app',
-    methods: 'GET, POST, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
