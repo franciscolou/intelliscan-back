da48a81dcedd8d67d306da91271e6298f70d426a
Francisco Lou <franciscolou@tecgraf.puc-rio.br>
2025-03-17 02:56:48 -0300

    OPTIONS method enabled in CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: 'https://intelliscan-inky.vercel.app',
-    methods: 'GET, POST, PATCH, DELETE',
+    methods: 'GET, POST, PATCH, DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
