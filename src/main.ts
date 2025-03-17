f0678c7eb88f047a963269a06a5f985624b55a1a
Francisco Lou <franciscogardenberg@gmail.com>
2025-03-17 13:33:48 -0300

    main.ts header format change

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    methods: ['GET', 'OPTIONS', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
