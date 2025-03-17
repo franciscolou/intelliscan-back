89d0b376dc529e1f3a1765160b855cd48e218ef9
Francisco Lou <franciscogardenberg@gmail.com>
2025-03-17 09:28:02 -0300

    Enable CORS generically

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://intelliscan-inky.vercel.app',
-    methods: 'GET, POST, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-
+  app.enableCors();
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
